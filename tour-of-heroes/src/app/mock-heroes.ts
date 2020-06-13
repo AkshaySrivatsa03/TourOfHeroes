e45184780abfc46ab3bf8aa74922d9fbc16366b1
ASrivatsa <akshay.srivatsa.03@gmail.com>
2020-06-13 17:15:52 -0600

    Added list of heroes and ability to select one

---
+++ tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from './hero';
+
+export const HEROES:Hero[] = [
+    {id: 1, name: 'WindRanger', type: 'Intelligence'},
+    {id: 2, name: 'Drow Ranger', type: 'Agility'},
+    {id: 3, name: 'Queen of Pain', type: 'Intelligence'},
+    {id: 4, name: 'Vengeful Spirit', type: 'Intelligence'},
+    {id: 5, name: 'Mirana', type: 'Agility'},
+    {id: 6, name: 'Medusa', type: 'Agility'},
+    {id: 7, name: 'Legion Commander', type: 'Strength'}
+]
