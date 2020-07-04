39d25ba0a23c0d452b0be8081ada671890d3ff6b
ASrivatsa <akshay.srivatsa.03@gmail.com>
2020-07-04 16:49:20 -0600

    Added search capability. Graduated, ready for more

--- tour-of-heroes/src/app/hero.service.ts
+++ tour-of-heroes/src/app/hero.service.ts
@@ -81,4 +81,16 @@ deleteHero(hero:Hero | number):Observable<Hero>{
     catchError(this.handleError<Hero>('deleteHero'))
   )
 }
+
+searchForHeroes(term: string): Observable<Hero[]>{
+  if (!term.trim()) {return of([]);}
+
+  return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+    tap(x => x.length ?
+      this.log(`found heroes matching "${term}"`) :
+      this.log(`no heroes matching "${term}"`)),
+   catchError(this.handleError<Hero[]>('searchForHeroes', []))
+ );
+}
+
 }
