e063ad2957213144c216e0f7b28eae111cd3cb9c
ASrivatsa <akshay.srivatsa.03@gmail.com>
2020-07-04 15:25:24 -0600

    Added ability to add new hero

--- tour-of-heroes/src/app/hero.service.ts
+++ tour-of-heroes/src/app/hero.service.ts
@@ -64,4 +64,11 @@ updateHero(hero: Hero): Observable<any> {
     catchError(this.handleError<any>('updateHero'))
   );
 }
+
+addHero(hero: Hero): Observable<Hero>{
+  return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+    tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+    catchError(this.handleError<Hero>('addHero'))
+  )
+}
 }
